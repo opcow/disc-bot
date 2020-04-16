31a94af29b7b057840d53c94748838b616b28c52
opcow <mitch.crane@gmail.com>
2020-04-16 03:35:31 -0400

    stop cron job before exiting

--- main.go
+++ main.go
@@ -70,13 +70,14 @@ func main() {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("Cronspec is %s\n", *cronSpec)
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
+	// Stop cron jobs.
+	reportCron.Stop()
 	// Cleanly close down the Discord session.
 	discord.Close()
 
