6781ac7a49c26e19158a3b44db57146876147bc0
opcow <mitch.crane@gmail.com>
2020-04-17 01:28:32 -0400

    moved covid funciont to another bot

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/robfig/cron/v3"
 )
 
 var lastCD time.Time
@@ -25,17 +24,9 @@ var seed = rand.NewSource(time.Now().Unix())
 var rnd = rand.New(seed)
 
 var (
-	dToken       = flag.String("t", "", "discord autentication token")
-	rToken       = flag.String("r", "", "rapidapi autentication token")
-	cronSpec     = flag.String("c", "0 1 * * *", "cron spec for periodic actions")
-	initialChans = flag.String("i", "", "comma separated string of initial channels to report to")
-	passwd       = flag.String("p", "", "password for the bot")
-	operators    = flag.String("o", "", "comma separated string of operators for the bot")
-	reportCron   *cron.Cron
-	reportCronID cron.EntryID
-	discord      *discordgo.Session
-	// channels for covid announcements
-	covChans map[string]struct{}
+	dToken    = flag.String("t", "", "discord autentication token")
+	operators = flag.String("o", "", "comma separated string of operators for the bot")
+	discord   *discordgo.Session
 	// bot operators
 	botOps map[string]struct{}
 	sc     chan os.Signal
@@ -43,11 +34,7 @@ var (
 
 func getEnv() {
 	*dToken = os.Getenv("DISCORDTOKEN")
-	*rToken = os.Getenv("RAPIDAPITOKEN")
-	*cronSpec = os.Getenv("DTCRONSPEC")
-	*initialChans = os.Getenv("DTCHANS")
 	*operators = os.Getenv("DTOPS")
-	*passwd = os.Getenv("DTPASSWD")
 }
 
 func main() {
@@ -60,10 +47,6 @@ func main() {
 		return
 	}
 
-	covChans = make(map[string]struct{})
-	for _, c := range strings.Split(*initialChans, ",") {
-		covChans[c] = struct{}{}
-	}
 	botOps = make(map[string]struct{})
 	for _, c := range strings.Split(*operators, ",") {
 		botOps[c] = struct{}{}
@@ -86,23 +69,12 @@ func main() {
 		return
 	}
 
-	reportCron = cron.New()
-	reportCronID, err = reportCron.AddFunc(*cronSpec, cronReport)
-	if err == nil {
-		reportCron.Start()
-	} else {
-		fmt.Println(err)
-	}
-
-	fmt.Printf("Cronspec is %s\n", *cronSpec)
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc = make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Stop cron jobs.
-	reportCron.Stop()
 	// Cleanly close down the Discord session.
 	discord.Close()
 }
@@ -140,88 +112,6 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if err == nil {
 			s.ChannelMessageSend(m.ChannelID, meta)
 		}
-	case "!cov": // report covid-19 stats
-		if *rToken == "" {
-			return
-		}
-		if time.Now().Sub(lastCD).Seconds() < 10 {
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Please wait %.0f seconds and try again.", 10.0-time.Now().Sub(lastCD).Seconds()))
-			return
-		}
-		var err error
-		var report string
-		lastCD = time.Now()
-		if len(msg) > 1 {
-			report, err = covid(strings.Join(msg[1:], "-"))
-		} else {
-			report, err = covid("usa")
-		}
-		if err == nil {
-			s.ChannelMessageSend(m.ChannelID, report)
-		}
-	case "!reaper": // periodic USA death toll reports
-		if len(msg) < 2 || msg[1] != "off" {
-			if !isOp(m.Author.ID) {
-				return
-			}
-			if len(msg) == 1 {
-				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Grim Reaper reports are *on* for %s.", chanIDtoMention(m.ChannelID)))
-				covChans[m.ChannelID] = struct{}{}
-			} else if id, err := chanLinkToID(msg[1]); err == nil {
-				covChans[id] = struct{}{}
-				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Grim Reaper reports are *on* for %s.", chanIDtoMention(id)))
-			}
-		} else if len(msg) == 2 {
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Grim Reaper reports are *off* for %s.", chanIDtoMention(m.ChannelID)))
-			delete(covChans, m.ChannelID)
-		} else if id, err := chanLinkToID(msg[2]); err == nil {
-			delete(covChans, id)
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Grim Reaper reports are *off* for %s.", chanIDtoMention(id)))
-		}
-	case "!chans":
-		if !isOp(m.Author.ID) {
-			return
-		}
-		for k := range covChans {
-			s.ChannelMessageSend(m.ChannelID, chanIDtoMention(k))
-		}
-	case "!op":
-		if !isOp(m.Author.ID) {
-			return
-		}
-		if len(msg) > 1 {
-			u, err := s.User(msg[1])
-			if err == nil {
-				botOps[u.ID] = struct{}{}
-			} else {
-				s.ChannelMessageSend(m.ChannelID, "Invalid user ID.")
-			}
-		} else {
-			botOps[m.Message.Author.ID] = struct{}{}
-		}
-	case "!deop":
-		if !isOp(m.Author.ID) {
-			return
-		}
-		if len(msg) > 1 {
-			if _, ok := botOps[msg[2]]; ok {
-				delete(botOps, msg[2])
-				u, err := s.User(msg[2])
-				if err != nil {
-					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("User ID %s removed from operator list.", msg[2]))
-				} else {
-					s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("User %s removed from operator list.", u.Mention()))
-				}
-			} else {
-				s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("User ID %s is not in the operator list.", msg[2]))
-			}
-		}
-	case "!delmsg":
-		if len(msg) > 2 {
-			s.ChannelMessageDelete(msg[1], msg[2])
-		}
-	case "!config":
-		showConfig(m.Author.ID)
 	case "!quit":
 		if isOp(m.Author.ID) && m.Message.GuildID == "" {
 			sc <- os.Kill
@@ -233,16 +123,7 @@ func showConfig(id string) {
 	if isOp(id) {
 		c, err := discord.UserChannelCreate(id)
 		if err == nil {
-			discord.ChannelMessageSend(c.ID, fmt.Sprintf("cronspec: %s", *cronSpec))
-			time.Sleep(time.Millisecond * 500)
-			// discord.ChannelMessageSend(c.ID, "channels:")
-			var s = "channels:"
-			for k := range covChans {
-				s = s + " " + chanIDtoMention(k)
-			}
-			discord.ChannelMessageSend(c.ID, s)
-			time.Sleep(time.Millisecond * 500)
-			s = "operators:"
+			s := "operators:"
 			for k := range botOps {
 				s = s + " " + userIDtoMention(k)
 			}
@@ -268,25 +149,6 @@ func userIDtoMention(id string) string {
 		return u.Mention()
 	}
 	return id
-}
-
-// Converts a channel ID to a mention. On error it returns the channel ID string.
-func chanIDtoMention(id string) string {
-	channel, err := discord.State.Channel(id)
-	if err == nil {
-		return channel.Mention()
-	}
-	return "channel: " + id
-}
-
-// Converts a channel link to an ID. If passed a valid ID it is returned it unchanged.
-func chanLinkToID(link string) (id string, err error) {
-	id = strings.Replace(strings.Replace(strings.Replace(link, "<", "", 1), ">", "", 1), "#", "", 1)
-	_, err = discord.Channel(id)
-	if err != nil {
-		return "", err
-	}
-	return id, nil
 }
 
 // "A wise– if perhaps slightly pedantic– generator of metaphor."
